0bed91e58163ebd22bda75f01bfc2cb2ec863962
unsurp4ssed <postbox6310@gmail.com>
2023-04-04 17:17:12 +0300

    hehe it is shit but it is shit that works

--- main.c
+++ main.c
@@ -2,11 +2,49 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <string.h>
+#include <stdbool.h>
+#include <math.h>
+#define PI 3.14
+#define FILENAME "C:\\Users\\admin\\CLionProjects\\flita\\dz2\\adjacency_matrix_7.txt"
+#define MAX_VERTICES 50
+#define OFFSET 50
 
-#define MAX_VERTICES 5
+typedef struct coords {
+    int x;
+    int y;
+} cd_t;
+
+cd_t getCoords(int sides, int n) {
+    cd_t A;
+    int r = 100;
+    A.x = r * cos(2 * PI * n / sides) + OFFSET;
+    A.y = r * sin(2 * PI * n / sides) + OFFSET;
+    return A;
+}
+
+void drawVertice(int n, int x, int y, HDC hdc, RECT rec ) {
+    Ellipse(hdc, x + 100, y + 100, x + 130, y + 130);
+    SetRect(&rec,x + 100, y + 100, x + 130, y + 130);
+    char VerticeNumber[2];
+    itoa(n,VerticeNumber, 10);
+    DrawText(hdc, TEXT(VerticeNumber),1, &rec, DT_TOP|DT_LEFT);
+}
 
 int matrix[MAX_VERTICES][MAX_VERTICES];
-const int vertices = 5;
+//const int vertices = 5;
+
+
+bool isBonded(int m[MAX_VERTICES][MAX_VERTICES], int vertices) {
+    int edges = 0;
+    for (int j = 0; j < vertices; j++) {
+        for (int i = 0; i < vertices; i++) {
+            if (i==j) continue;
+            if (m[i][j]) edges++;
+        }
+    }
+    if (edges > ((vertices -1 ) * (vertices - 2)) / 2) return true;
+    else return false;
+}
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
@@ -19,6 +57,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wc.hbrBackground = (HBRUSH)(COLOR_BACKGROUND);
     wc.lpszClassName = "MyClass";
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+    SystemParametersInfoA(SPI_SETFONTSMOOTHING, TRUE, NULL, 0); //antialiasing!!!!!
+    //it doesnt work tho...
+
     if(!RegisterClass(&wc))
     {
         printf("This Program Requires Windows NT\n");
@@ -33,7 +75,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
                         640, 480,
                         NULL, NULL, hInstance, NULL);
 
-    FILE* file = fopen("C:\\Users\\admin\\CLionProjects\\flita\\dz2\\adjacency_matrix_5.txt", "r");
+    FILE* file = fopen(FILENAME, "r");
+
+
     if (file == NULL) {
         MessageBox(NULL, "Could not open file.", "Error", MB_ICONERROR);
         return 1;
@@ -44,17 +88,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     UpdateWindow(hwnd);
     ShowWindow(hwnd, SW_NORMAL);
 
-    char str[12];
+    char str[MAX_VERTICES];
     char element[2];
     const char sep[2] = " ";
     char *token;
 
-    for (int j = 0; j < 5; j++) {
-        fgets(str, 12, file);
+    int vertices = (int)strlen(fgets(str, MAX_VERTICES, file))/2;
+    printf("\n\t%d\n", vertices);
+    fclose(file);
+    file = fopen(FILENAME, "r");
+
+    for (int j = 0; j < vertices; j++) {
+        fgets(str, MAX_VERTICES, file);
         token = strtok(str, sep);
         strcpy(element, token);
         matrix[0][j] = atoi(element);
-        for (int i = 1; i < 5; i++) {
+        for (int i = 1; i < vertices; i++) {
             token = strtok(NULL, sep);
             strcpy(element, token);
             matrix[i][j] = atoi(element);
@@ -69,6 +118,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         printf("\n");
     }
 
+    if (isBonded(matrix, vertices)) {
+        MessageBox(NULL, "Graph is bonded", "INFO", MB_ICONINFORMATION);
+    }
+    else MessageBox(NULL, "Graph is not bonded", "INFO", MB_ICONINFORMATION);
 
 
     MSG msg;
@@ -88,38 +141,67 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             RECT rec;
             HPEN hpen = NULL;
 
-            int x[5] = {100, 195, 5, 159, 41};
-            int y[5] = {200, 131, 131, 19, 19};
-            Ellipse(hdc, x[0] + 10, y[0] + 10, x[0] + 40, y[0] + 40);
-            SetRect(&rec,x[0] + 10, y[0] + 20, x[0] + 40, y[0] + 50);
-            DrawText(hdc, TEXT("1"),1, &rec, DT_TOP|DT_LEFT);
-            Ellipse(hdc, x[1] + 10, y[1] + 10, x[1] + 40, y[1] + 40);
-            SetRect(&rec,x[1] + 10, y[1] + 10, x[1] + 40, y[1] + 40);
-            DrawText(hdc, TEXT("2"),1, &rec, DT_TOP|DT_LEFT);
-            Ellipse(hdc, x[2] + 10, y[2] + 10, x[2] + 40, y[2] + 40);
-            SetRect(&rec,x[2] + 10, y[2] + 10, x[2] + 40, y[2] + 40);
-            DrawText(hdc, TEXT("3"),1, &rec, DT_TOP|DT_LEFT);
-            Ellipse(hdc, x[3] + 10, y[3] + 10, x[3] + 40, y[3] + 40);
-            SetRect(&rec,x[3] + 10, y[3] + 10, x[3] + 40, y[3] + 40);
-            DrawText(hdc, TEXT("4"),1, &rec, DT_TOP|DT_LEFT);
-            Ellipse(hdc, x[4] + 10, y[4] + 10, x[4] + 40, y[4] + 40);
-            SetRect(&rec,x[4] + 10, y[4] + 10, x[4] + 40, y[4] + 40);
-            DrawText(hdc, TEXT("5"),1, &rec, DT_TOP|DT_LEFT);
+
+            char str[MAX_VERTICES];
+            FILE* file = fopen(FILENAME, "r");
+            int vertices = (int)strlen(fgets(str, MAX_VERTICES, file))/2;
+            fclose(file);
+
+            cd_t A;
+
+            int* x = (int*) malloc(vertices*sizeof(int));
+            int* y = (int*) malloc(vertices*sizeof(int));
 
             for (int i = 0; i < vertices; i++) {
+                A = getCoords(vertices, i+1);
+                x[i] = A.x; y[i] = A.y;
+                printf("%d:%d ", A.x, A.y);
+                drawVertice(i+1, A.x, A.y, hdc, rec);
+            }
+            printf("\n");
+
+
+
+            //int x[5] = {100, 195, 5, 159, 41};
+            //int y[5] = {200, 131, 131, 19, 19};
+            //Ellipse(hdc, x[0] + 10, y[0] + 10, x[0] + 40, y[0] + 40);
+            //SetRect(&rec,x[0] + 10, y[0] + 20, x[0] + 40, y[0] + 50);
+            //DrawText(hdc, TEXT("1"),1, &rec, DT_TOP|DT_LEFT);
+            //drawVertice(1, x[0], y[0], hdc, rec);
+            //Ellipse(hdc, x[1] + 10, y[1] + 10, x[1] + 40, y[1] + 40);
+            //SetRect(&rec,x[1] + 10, y[1] + 10, x[1] + 40, y[1] + 40);
+            //DrawText(hdc, TEXT("2"),1, &rec, DT_TOP|DT_LEFT);
+            //drawVertice(2, x[1], y[2], hdc, rec);
+
+            //Ellipse(hdc, x[2] + 10, y[2] + 10, x[2] + 40, y[2] + 40);
+            //SetRect(&rec,x[2] + 10, y[2] + 10, x[2] + 40, y[2] + 40);
+            //DrawText(hdc, TEXT("3"),1, &rec, DT_TOP|DT_LEFT);
+            //Ellipse(hdc, x[3] + 10, y[3] + 10, x[3] + 40, y[3] + 40);
+            //SetRect(&rec,x[3] + 10, y[3] + 10, x[3] + 40, y[3] + 40);
+            //DrawText(hdc, TEXT("4"),1, &rec, DT_TOP|DT_LEFT);
+            //Ellipse(hdc, x[4] + 10, y[4] + 10, x[4] + 40, y[4] + 40);
+            //SetRect(&rec,x[4] + 10, y[4] + 10, x[4] + 40, y[4] + 40);
+            //DrawText(hdc, TEXT("5"),1, &rec, DT_TOP|DT_LEFT);
+
+            for (int i = 0; i < vertices; i++) {
+                printf("%d:%d ", x[i], y[i]);
                 for (int j = 0; j < vertices; j++) {
+
                     if (matrix[i][j] != 0) {
+
+
                         hpen = CreatePen(PS_SOLID, matrix[i][j], RGB(0, 0, 0));
                         SelectObject(hdc, hpen);
-                        MoveToEx(hdc, x[i] + 25, y[i] + 25, NULL);
-                        LineTo(hdc, x[j] + 25, y[j] + 25);
+                        MoveToEx(hdc, x[i] + 115, y[i] + 115, NULL);
+                        LineTo(hdc, x[j] + 115, y[j] + 115);
                         if (i == j) {
-                            AngleArc(hdc, x[i] + 25, y[i] + 25, 50, 30, 50);
-                            LineTo(hdc, x[i] + 25, y[i] + 25);
+                            AngleArc(hdc, x[i] + 115, y[i] + 115, 50, 30, 50);
+                            LineTo(hdc, x[i] + 115, y[i] + 115);
                         }
                     }
                 }
             }
+            free(x); free(y);
             EndPaint(hwnd, &ps);
             DeleteObject(hpen);
             GetStockObject(DC_PEN);
